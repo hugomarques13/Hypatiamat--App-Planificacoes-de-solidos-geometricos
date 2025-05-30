68a8dffba5625df5daf283ab87ea5e325168d808
DiegoBangles <gugu.sm13@gmail.com>
2025-05-30 14:30:25 +0100

    Adicionado voltar ao quiz

--- src/scenes/Cone.js
+++ src/scenes/Cone.js
@@ -19,6 +19,7 @@ export default class Cone extends Phaser.Scene {
     this.load.image('bt_fullscreen', 'assets/bt_fullscreen.png');
     this.load.image('bt_info', 'assets/bt_info.png');
     this.load.image('bt_voltar', 'assets/bt_voltar.png');
+    this.load.image('bt_butaoVazio', 'assets/bt_butaoVazio.png');
   }
 
   create() {
@@ -47,6 +48,40 @@ export default class Cone extends Phaser.Scene {
     this.addHoverEffect(btnFullScreen);
     this.addHoverEffect(btnBack);
     this.addHoverEffect(btnInfo);
+
+    if (this.scene.settings.data?.returnToQuiz) {
+      const quizData = this.scene.settings.data;
+      
+      this.returnButtonContainer = this.add.container(512, 520);
+      
+      const returnBtnBg = this.add.image(0, 0, 'bt_butaoVazio')
+          .setScale(0.23)
+          .setInteractive({ useHandCursor: true });
+      
+      const returnBtnText = this.add.text(0, -4, "Voltar ao Quiz", {
+          fontSize: '20px',
+          fontFamily: 'Snap ITC',
+          color: '#993300',
+          align: 'center'
+      }).setOrigin(0.5);
+      
+      this.addHoverEffect(returnBtnBg, returnBtnText);
+      
+      this.returnButtonContainer.add([returnBtnBg, returnBtnText]);
+      
+      returnBtnBg.on('pointerup', () => {
+          this.cleanupDOM();
+          this.scene.stop();
+          
+          // Restart the QuizScene with preserved data
+          this.scene.start('QuizScene', {
+              returnToQuiz: true,
+              currentQuestionIndex: quizData.nextQuestionIndex + 1,
+              score: quizData.currentScore,
+              questions: quizData.questions
+          });
+      });
+    }
 
     btnHome.on('pointerup', () => {
       this.cleanupDOM();
@@ -709,6 +744,10 @@ export default class Cone extends Phaser.Scene {
 
   cleanupDOM() {
     // Remove Three.js canvas
+    if (this.scene.settings.data) {
+        this.scene.settings.data = null;
+    }
+
     if (this.threeCanvas?.parentNode) {
       this.threeCanvas.remove();
       this.threeCanvas = null;
